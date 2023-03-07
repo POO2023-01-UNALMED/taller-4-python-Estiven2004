e28e10434ce817059cbc393f4693ee366a8f77b4
Estiven2004 <eospinaj@unal.edu.co>
2023-03-07 08:53:42 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -19,7 +19,7 @@ class Grupo:
         self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
-        return f'{self._nombre} {self._salon}'
+        return f'Grupo de estudiantes: {self._grupo}'
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
